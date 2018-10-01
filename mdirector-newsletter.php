ef2a3eb777d6cc265688644fca423a0187cc8b3c
Alex <aperez@antevenio.com>
2018-10-01 11:37:33 +0200

    upgrading version

--- mdirector-newsletter.php
+++ mdirector-newsletter.php
@@ -10,7 +10,7 @@
  * Plugin Name:       MDirector Newsletter
  * Plugin URI:        http://www.mdirector.com/
  * Description:       Official MDirector plugin for wordpress. Add MDirector sign-up forms to your WordPress site.
- * Version:           2.0.9
+ * Version:           2.1.0
  * Author:            MDirector
  * Author URI:        http://mdirector.com/
  * License:           GPL-2.0+
